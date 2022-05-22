e8a24550991c334759bd962cc1667c37d16547a5
Marcos Vinicius <marcosab.vinicius@gmail.com>
2022-05-22 16:07:03 -0300

    aws configs

---
+++ src/main/java/com/s3example/demo/adapters/service/S3Service.java
@@ -0,0 +1,10 @@
+package com.s3example.demo.adapters.service;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class S3Service {
+
+
+
+}
---
+++ src/main/java/com/s3example/demo/config/AWSConfig.java
@@ -0,0 +1,25 @@
+package com.s3example.demo.config;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AWSConfig {
+
+    AWSCredentials credentials = new BasicAWSCredentials(
+            "accesskey",
+            "secretkey"
+    );
+
+    AmazonS3 s3client = AmazonS3ClientBuilder
+            .standard()
+            .withCredentials(new AWSStaticCredentialsProvider(credentials))
+            .withRegion(Regions.US_EAST_1)
+            .build();
+
+}
